b63e39a5cf28e359ffcd453d98da5f75a7240d05
javregvar <jvireg@gmail.com>
2016-11-19 14:14:06 +0100

    Create myClass.java
    Probando tests con junit desde pc a repositorio git

---
+++ myClass.java
@@ -0,0 +1,11 @@
+public class myClass(){
+  public myClass(){};
+  
+  public void muestraMensaje(String msg){
+    System.out.println(msg);
+  }
+  
+  public suma(int a, int b){
+    return a+b;
+  }
+}
